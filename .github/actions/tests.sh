1798f74254c49c0a9058b809a9768d2b2d24a3e6
angelop-1602 <a.peralta0216@gmail.comm>
2026-09-23 14:17:28 +0800

    fix: point theme CI at SPUP plugin identity

--- .github/actions/tests.sh
+++ .github/actions/tests.sh
@@ -2,4 +2,4 @@
 
 set -e
 echo "Run cypress tests"
-npx cypress run  --config '{"specPattern":["plugins/themes/classic/cypress/tests/functional/*.cy.js"]}'
+npx cypress run  --config '{"specPattern":["plugins/themes/spupLightExplorerTheme/cypress/tests/functional/*.cy.js"]}'
